2c4ffc7fb44bb0603a8f2c5d0a0dbf993978844f
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-03 00:49:35 +0400

    installRedux

---
+++ store/todoSlice/index.ts
@@ -0,0 +1,45 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+
+interface TodoItem {
+    id:number;
+    text:string
+}
+
+interface TodoState {
+    todo:TodoItem[]
+}
+
+const initialState: TodoState = {
+    todo:[]
+}
+console.log(initialState);
+
+const todoSlice = createSlice({
+    name:"todoSlice",
+    initialState,
+    reducers:{
+
+        fillToDo:(state, action:PayloadAction<TodoItem[]>) => {
+            state.todo = [...action.payload]
+        },
+
+        addTodo:(state, action:PayloadAction<TodoItem>)=>{
+            state.todo = [action.payload, ...state.todo ]
+            
+        },
+
+        rmvTodo:(state, action:PayloadAction<TodoItem["id"]>)=>{
+
+            const newToDo = state.todo.filter((item) => item.id !== action.payload)
+            state.todo = newToDo
+        },
+       
+    }
+})
+
+
+export const {fillToDo, addTodo,rmvTodo} = todoSlice.actions
+
+
+export default todoSlice.reducer
